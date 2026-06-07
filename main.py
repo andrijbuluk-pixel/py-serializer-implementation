ea25b194a11e11d3c2afd30afdd5b86b5fd2b684
Andriy <andrijbuluk@gmail.com>
2026-06-07 15:59:15 +0200

    Aded: functions for serialize_car, deserialize_car

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import io
+
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializers = CarSerializer(car)
+    return JSONRenderer().render(serializers.data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    json = io.BytesIO(json)
+    parser = JSONParser().parse(json)
+    serializer = CarSerializer(data=parser)
+    serializer.is_valid(raise_exception=True)
+    instance = serializer.save()
+    return instance
